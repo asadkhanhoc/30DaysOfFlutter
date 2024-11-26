8948220439b46907cee10bc090aa63bf32fc0242
asadkhanhoc <asadkhanhoc@gmail.com>
2024-11-26 21:05:41 +0500

    Day2

---
+++ basic/lib/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int days = 10;
+    String name = "Asad";
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        foregroundColor: Colors.white,
+        title: const Text("First"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to  $days of flutter by $name"),
+        ),
+      ),
+      drawer: const Drawer(
+        elevation: 15,
+      ),
+    );
+  }
+}
